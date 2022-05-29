1e5ad6147d9c9f45edc755c7e04409460ee41df2
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-05-29 12:11:07 -0300

    added sideBar and form

---
+++ src/components/form.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div class="box">
+		<div class="columns">
+			<div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+				<input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+			</div>
+			<div class="column">
+				<div class="is-flex is-align-items-center is-justify-content-space-between">
+					<section class="mr-2">
+						<strong>00:00:00</strong>
+					</section>
+					<button class="button mr-2">
+						<span class="icon ml-0">
+							<i class="fas fa-play"></i>
+						</span>
+						<span class="ml-2">play</span>
+					</button>
+					<button class="button">
+						<span class="icon ml-0">
+							<i class="fas fa-stop"></i>
+						</span>
+						<span class="ml-2">stop</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+</style>
---
+++ src/components/sideBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<header>
+		<h1>
+			<img src="../assets/logo.png" alt="Alura Tracker">
+		</h1>
+	</header>
+</template>
+
+<style>
+header {
+	width: 100%;
+	height: 100vh;
+	background: #0d3b66;
+	padding: 1rem;
+}
+@media only screen and (max-width: 768px) {
+	header {
+		padding: 2.5rem;
+		height: auto;
+	}
+}
+</style>
