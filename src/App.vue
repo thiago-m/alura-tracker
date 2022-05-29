a507a3ee5829730e4f74de343ee64b255f3545bd
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-05-29 12:10:58 -0300

    added logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+	import SideBar from './components/sideBar.vue'
+	import Form from './components/form.vue'
 </script>
 
 <template>
+	<main class="columns is-gapless is-multiline">
+		<div class="column is-one-quarter">
+			<sideBar />
+		</div>
+		<div class="column is-three-quarter">
+			<Form />
+		</div>
+	</main>
 </template>
 
 <style>
