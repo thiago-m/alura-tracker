dcc8a38baaac7769700856e7e8bb10d88e433e80
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-05-30 08:34:17 -0300

    added component MeButton

---
+++ src/components/MeButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+
+defineProps({
+	icon: {
+		type: String,
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	disabled: Boolean
+})
+
+const emit = defineEmits<{
+	(e: 'click'): void
+}>()
+
+const clicker = (): void => {
+	emit('click')
+}
+
+</script>
+
+<template>
+	<button class="button" @click="clicker" :disabled="disabled">
+		<span class="icon">
+			<i :class="icon"></i>
+		</span>
+		<span class="ml-2">{{text}}</span>
+	</button>
+</template>
