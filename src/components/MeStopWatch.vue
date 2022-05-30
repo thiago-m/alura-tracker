2ab544404f777bb60dd3f1f76eec195140c8c4fc
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-05-30 08:34:39 -0300

    added component MeStopWatch

---
+++ src/components/MeStopWatch.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	import { computed, defineProps } from 'vue'
+
+	const props = defineProps({
+		timeSeconds: {
+			type: Number,
+			default: 0
+		}
+	})
+
+	const formatTime = computed(
+		(): string => new Date(props.timeSeconds * 1000).toISOString().substr(11,8)
+	)
+</script>
+
+<template>
+	<section class="mr-2">
+		<strong>{{formatTime}}</strong>
+	</section>
+</template>
