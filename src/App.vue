e78306ab24119f070d37b0e9ee887d96a1e04e29
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-05-30 08:36:12 -0300

    updated app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
-	import MeSideBar from './components/MeSideBar.vue'
-	import MeForm from './components/MeForm.vue'
+import { ref } from 'vue'
+import MeSideBar from './components/MeSideBar.vue'
+import MeForm from './components/MeForm.vue'
+import MeTask from './components/MeTask.vue'
+import ITask from './interfaces/ITask'
+
+const tasks = ref<ITask[]>([])
+
+const saveTask = (task: ITask): void => {
+	tasks.value.push(task)
+}
 </script>
 
 <template>
@@ -9,10 +18,23 @@
 			<MeSideBar />
 		</div>
 		<div class="column is-three-quarter">
-			<MeForm />
+			<MeForm @save="saveTask" />
+			<div class="list">
+				<div class="box has-text-weight-bold" v-if="!tasks.length">
+					<div class="columns">
+						<div class="column is-7">
+							Nenhuma tarefa ainda
+						</div>
+					</div>
+				</div>
+				<MeTask v-else v-for="(task, index) in tasks" :key="`${index}`" :task="task" />
+			</div>
 		</div>
 	</main>
 </template>
 
 <style>
+.list {
+	padding: 1.25rem;
+}
 </style>
